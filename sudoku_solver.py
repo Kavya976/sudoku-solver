4c7dfdd0e36164a0b613acc82042231f66eb230a
Kavya976 <Kavyaaleti2021@gmail.com>
2026-05-16 13:16:35 +0530

    Create sudoku_solver.py

---
+++ sudoku_solver.py
@@ -0,0 +1,65 @@
+def is_valid(board, row, col, num):
+    if num in board[row]:
+        return False
+    
+    for i in range(9):
+        if board[i][col] == num:
+            return False
+    
+    box_row = (row // 3) * 3
+    box_col = (col // 3) * 3
+    for i in range(3):
+        for j in range(3):
+            if board[box_row + i][box_col + j] == num:
+                return False
+    
+    return True
+
+
+def solve_sudoku(board):
+    for row in range(9):
+        for col in range(9):
+            if board[row][col] == 0:
+                for num in range(1, 10):
+                    if is_valid(board, row, col, num):
+                        board[row][col] = num
+                        if solve_sudoku(board):
+                            return True
+                        board[row][col] = 0
+                return False
+    return True
+
+
+def print_board(board):
+    for i in range(9):
+        if i % 3 == 0 and i != 0:
+            print("- - - - - - - - - - -")
+        for j in range(9):
+            if j % 3 == 0 and j != 0:
+                print("| ", end="")
+            if j == 8:
+                print(board[i][j])
+            else:
+                print(str(board[i][j]) + " ", end="")
+
+
+board = [
+    [5, 3, 0, 0, 7, 0, 0, 0, 0],
+    [6, 0, 0, 1, 9, 5, 0, 0, 0],
+    [0, 9, 8, 0, 0, 0, 0, 6, 0],
+    [8, 0, 0, 0, 6, 0, 0, 0, 3],
+    [4, 0, 0, 8, 0, 3, 0, 0, 1],
+    [7, 0, 0, 0, 2, 0, 0, 0, 6],
+    [0, 6, 0, 0, 0, 0, 2, 8, 0],
+    [0, 0, 0, 4, 1, 9, 0, 0, 5],
+    [0, 0, 0, 0, 8, 0, 0, 7, 9]
+]
+
+print("Sudoku Puzzle:")
+print_board(board)
+
+if solve_sudoku(board):
+    print("\nSolved Sudoku:")
+    print_board(board)
+else:
+    print("No solution exists.")
